675fcb74aa661632262f93ad32fa7231c907e397
joicebrl <joicerezendebrl@gmail.com>
2025-04-27 09:53:51 -0300

    feat: adiciona loop inicial

--- ValidadorDeSenha.java
+++ ValidadorDeSenha.java
@@ -1,1 +1,18 @@
 import java.util.Scanner;
+
+public class ValidadorDeSenha {
+    public static void main(String[] args) {
+        String senha = "";
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            boolean validacao = true;
+
+            System.out.println("Rodou");
+
+            if (validacao == true) {
+                break;
+            }
+        } while (true);
+    }
+}
